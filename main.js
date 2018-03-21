1af361b2d098d90d146a5d840f99ed271cfd0458
kartiktennis <krishnankartik6@gmail.com>
2018-03-21 13:05:14 -0400

    null check for input field values

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-var address = document.getElementById("address").value 
-var zipcode = document.getElementById("zipcode").value
 
-//create array of valid zipcodes    
+
+
+var address, 
+element = document.getElementById("address");
+var zipcode,
+element2 = document.getElementById("zipcode");
+
+//null check for input field values
+if (element != null || element2 != null) {
+    address = element; 
+    zipcode = element2;
+}
+
+var address_user = address.elements["user_address"]; 
+//create array of valid zipcodes
+console.log(address_user);
